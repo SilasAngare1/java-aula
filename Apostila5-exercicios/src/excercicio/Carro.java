ee30cc1b39c120531ce69672a87bbbc26f831309
Laboratório FIAP <labsfiap@fiap.com.br>
2026-04-08 20:55:29 -0300

    commit do exercicio

---
+++ Apostila5-exercicios/src/excercicio/Carro.java
@@ -0,0 +1,12 @@
+package excercicio;
+
+public class Carro {
+
+    String modelo;
+    int quantidadeLugares;
+    int quantidadePortas;
+    double comprimento;
+    int anoFabricacao;
+    Cor cor;
+
+}
---
+++ Apostila5-exercicios/src/excercicio/Cor.java
@@ -0,0 +1,15 @@
+package excercicio;
+
+public class Cor {
+    String nome;
+    int r;
+    int g;
+    int b;
+
+   public void alterarCor(int r, int b, int g, String nome){
+
+
+   }
+
+
+}
